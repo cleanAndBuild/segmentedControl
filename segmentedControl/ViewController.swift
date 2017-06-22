4c5d70a73215e4d3f4358876ae359bfa33e26b9b
Kobayashi Yako <yako.koba@gmail.com>
2017-06-23 00:30:57 +0900

    Initial Commit

---
+++ segmentedControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  segmentedControl
+//
+//  Created by Kobayashi Yako on 2017/06/23.
+//  Copyright © 2017年 Yako Kobayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
